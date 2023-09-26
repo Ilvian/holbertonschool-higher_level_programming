bfbe11716779ab368d7483de1f561ceb169c2a4c
Ilvian <d.ilvian1993@gmail.com>
2023-09-26 08:48:53 -0700

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-sum = 0
 if __name__ == "__main__":
-    for num in argv[1:]:
-        sum += int(num)
-print(sum)
+    import sys
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print("{}".format(sum))
