d5d619f38effafcf890ee38b05b9c54ad7923bfd
Ilvian <d.ilvian1993@gmail.com>
2023-09-26 09:08:26 -0700

    task 4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-
-def main():
-    argv = sys.argv[1:]
-    num_args = len(argv)
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-        print(f"{num_args} argument:")
-        print(f"1: {argv[0]}")
+        print("1 argument:")
     else:
         print(f"{num_args} arguments:")
-        for i in range(num_args):
-            print(f"{i + 1}: {argv[i]}")
-
-
-if __name__ == "__main__":
-    main()
+    for i in range(num_args):
+        print(f"{i + 1}: {sys.argv[i + 1]}")
