62b62303f2dc86ac88f2ca2ed2205dff11fdbe5a
Ilvian <d.ilvian1993@gmail.com>
2023-10-02 09:11:58 -0700

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    max_width = 0
     for row in matrix:
-        for num in row:
-            max_width = max(max_width, len(str(num)))
-    for row in matrix:
-        for num in row:
-            print("{:>{width}}".format(num, width=max_width), end=" ")
+        for columns in row:
+            print("{:d}".format(columns), end="")
+            if row.index(columns) != len(row) - 1:
+                print(" ".format(), end="")
         print()
