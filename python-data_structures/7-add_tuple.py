eb7441230f0dbd9ac1df6e381922691711c75e83
Ilvian <135588769+Ilvian@users.noreply.github.com>
2023-10-03 00:45:45 +0200

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    list_a = (list(tuple_a) + [0, 0])[:2]
-    list_b = (list(tuple_b) + [0, 0])[:2]
-    return list_a[0] + list_b[0], list_a[1] + list_b[1]
+    a = (list(tuple_a) + [0, 0])[:2]
+    b = (list(tuple_b) + [0, 0])[:2]
+    return a[0] + b[0], a[1] + b[1]
