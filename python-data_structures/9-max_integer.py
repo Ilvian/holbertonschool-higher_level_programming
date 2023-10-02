5904253e7446ddbb7df33244a7f9db43c3c17585
Ilvian <d.ilvian1993@gmail.com>
2023-10-02 05:53:13 -0700

    task 10

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     higher = my_list[0]
-    for num in my_list:
-        if num > higher:
-            higher = num
+    for i in my_list:
+        if i > higher:
+            higher = i
     return higher
