fb9e993141a924d7463c50ca1bacf799fdb67d1d
Ilvian <d.ilvian1993@gmail.com>
2023-10-02 15:21:27 -0700

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2, b1, b2 = 0, 0, 0, 0
-    if len(tuple_a) > 0:
-        a1 = tuple_a[0]
-    if len(tuple_a) > 1:
-        a2 = tuple_a[1]
-    if len(tuple_b) > 0:
-        b1 = tuple_b[0]
-    if len(tuple_b) > 1:
-        b2 = tuple_b[1]
-    sum1 = a1 + b1
-    sum2 = a2 + b2
-    return (sum1, sum2)
+    list_a = (list(tuple_a) + [0, 0])[:2]
+    list_b = (list(tuple_b) + [0, 0])[:2]
+    return list_a[0] + list_b[0], list_a[1] + list_b[1]
