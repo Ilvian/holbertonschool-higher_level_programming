6331e4d1d401b65186254dd05b3cf17879b54db3
Ilvian <d.ilvian1993@gmail.com>
2023-10-09 03:20:27 -0700

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -20,6 +20,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
