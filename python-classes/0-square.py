0703dfa7f1e127732f2196204ed64c5ff46f7a0c
Ilvian <d.ilvian1993@gmail.com>
2023-10-09 02:12:15 -0700

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-"""
-This is the documentation for the Square class.
-The class represents a geometric square and associated operations.
-"""
 class Square:
+    """
+    This is the documentation for the Square class.
+    The class represents a geometric square and associated operations.
+    """
     pass
