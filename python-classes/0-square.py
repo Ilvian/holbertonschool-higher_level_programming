0e87086de4535a3dbd284d4f407ed7b41d6aecae
Ilvian <d.ilvian1993@gmail.com>
2023-10-09 02:18:26 -0700

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
 class Square:
     """
     This is the documentation for the Square class.
