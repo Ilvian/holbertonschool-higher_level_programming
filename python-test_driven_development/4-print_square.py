30f93252acffefd202cb3dfc91429619a7d06588
Ilvian <d.ilvian1993@gmail.com>
2023-10-10 04:29:41 -0700

    task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def print_square(size):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
