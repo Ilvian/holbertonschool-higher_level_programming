a869a9c1afc0afd89be391b03f22efaa083fadb6
Ilvian <d.ilvian1993@gmail.com>
2023-10-10 04:26:35 -0700

    task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
