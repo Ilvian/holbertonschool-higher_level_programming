551e90a7b49fde69a2876ca4eb043b421603b387
Ilvian <d.ilvian1993@gmail.com>
2023-10-10 04:32:11 -0700

    tasks

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
 def text_indentation(text):
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     i = 0
