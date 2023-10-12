6bffec4f1eaf40d56fa4db94ee02597142da9695
Ilvian <d.ilvian1993@gmail.com>
2023-10-11 19:39:03 -0700

    task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""
+
+
 class Rectangle:
+    """
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
     pass
