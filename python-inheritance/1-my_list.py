cdbf0cadad1a4fdfa125175a017b88b95fce430b
Ilvian <d.ilvian1993@gmail.com>
2023-10-17 08:53:23 -0700

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -13,4 +13,6 @@ class MyList(list):
         'print(__import__("my_module").my_function.__doc__)'
         python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
         """
+        sorted_list = sorted(self)
         print(sorted(self))
+        return sorted_list
