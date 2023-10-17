24f63573b4d0b16447552ba786a2a1a81c23a85c
Ilvian <d.ilvian1993@gmail.com>
2023-10-17 08:08:05 -0700

    task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,35 +4,7 @@ python3 -c 'print(__import__("my_module").__doc__)'
 """
 
 
-class BaseGeometry:
-    """
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-    """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
