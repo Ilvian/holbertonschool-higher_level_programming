3afb65e3c9b204d17b7b7bfe4d17087fe2943888
Ilvian <d.ilvian1993@gmail.com>
2023-10-17 09:40:24 -0700

    task 0-5

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    return json.loads(my_str)
---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    'print(__import__("my_module").my_function.__doc__)'
+    python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    """
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
