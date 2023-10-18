38c9cc1be01cfb62110a74d48c065c28f53c6704
Ilvian <d.ilvian1993@gmail.com>
2023-10-18 14:12:42 -0700

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,15 +6,17 @@ python3 -c 'print(__import__("my_module").__doc__)'
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    coll_data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    coll_data = []
+    try:
+        coll_data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        coll_data = []
 
-for arg in sys.argv[1:]:
-    coll_data.append(arg)
+    for arg in sys.argv[1:]:
+        coll_data.append(arg)
 
-save_to_json_file(coll_data, "add_item.json")
+    save_to_json_file(coll_data, "add_item.json")
