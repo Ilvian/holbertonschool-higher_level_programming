21c30fa67d8ce1617e6fb7f2706c8568dc5297f8
Ilvian <d.ilvian1993@gmail.com>
2023-10-23 05:29:24 -0700

    task 12

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,6 +108,19 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+    def to_dictionary(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
     def __str__(self):
         """
         python3 -c 'print(__import__("my_module").my_function.__doc__)'
