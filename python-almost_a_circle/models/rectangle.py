b17533a9c55476cc8d7ba26a02bfd8a101374aa8
Ilvian <d.ilvian1993@gmail.com>
2023-10-23 02:54:10 -0700

    task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -69,4 +69,8 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         return self.width * self.height
