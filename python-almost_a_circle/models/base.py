a1e432fdce150a88f7192111d811c89e32ea77cf
Ilvian <d.ilvian1993@gmail.com>
2023-10-23 02:12:25 -0700

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,7 +9,12 @@ class Base:
     python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
         if id is not None:
             self.id = id
         else:
