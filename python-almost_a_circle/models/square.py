56218dc39f15fd4e71fc60fd12ce7a24912d8816
Ilvian <d.ilvian1993@gmail.com>
2023-10-23 04:41:59 -0700

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
