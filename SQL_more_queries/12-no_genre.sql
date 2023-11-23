af6460c264193bf006d12b85db4e3fc000ea2351
Ilvian <d.ilvian1993@gmail.com>
2023-11-23 00:27:43 -0800

    task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON v_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
