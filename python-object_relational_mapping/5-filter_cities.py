a91f40f5896b41c02c2c71c166d882b619007e24
Ilvian <d.ilvian1993@gmail.com>
2023-11-29 04:59:30 -0800

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,26 +9,23 @@ using the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
-
-if __name__ == "__name__":
+if __name__ == '__main__':
     db = MySQLdb.connect(
-            host="localhost",
+            host='localhost',
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
             )
     cur = db.cursor()
-    with db.cursor() as cur:
-        cur.execute("SELECT cities.id, cities.name FROM cities \
-            JOIN states ON cities.state_id = states.id \
-            WHERE states.name LIKE BINARY %(state_name)s \
-            ORDER BY cities.id ASC", {'state_name': sys.argv[4]})
+    que = "SELECT cities.name FROM cities\
+                    JOIN states ON states.id=cities.state_id\
+                    WHERE states.name LIKE BINARY %s\
+                    ORDER BY cities.id ASC"
+    cur.execute(que, (sys.argv[4], ))
+    rows = cur.fetchall()
 
-        rows = cur.fetchall()
-
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+    print(", ".join([row[0] for row in rows]))
 
     cur.close()
     db.close()
