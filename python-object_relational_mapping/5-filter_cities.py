406f9feb9e2847523e0f1887ced85fb1fa1ccf44
Ilvian <d.ilvian1993@gmail.com>
2023-11-29 04:52:08 -0800

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,12 +19,16 @@ if __name__ == "__name__":
             port=3306
             )
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states \
-            ON states.id=cities.state_id WHERE states.name LIKE BINARY %s\
-            ORDER BY cities.id ASC", (sys.argv[4]))
-    rows = cur.fetchall()
+    with db.cursor() as cur:
+        cur.execute("SELECT cities.id, cities.name FROM cities \
+            JOIN states ON cities.state_id = states.id \
+            WHERE states.name LIKE BINARY %(state_name)s \
+            ORDER BY cities.id ASC", {'state_name': argv[4]})
+
+        rows = cur.fetchall()
+
     if rows is not None:
-        print(", ".join(row[0] for row in rows))
+        print(", ".join([row[1] for row in rows]))
 
     cur.close()
     db.close()
